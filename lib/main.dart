bcb181f2d7c81d8ba5a050b13c06d97de73c34e6
고희청 <ghc2gg@naver.com>
2024-05-24 12:36:25 +0900

    main 수정중

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+
+      // main.dart 추가함
       theme: ThemeData(
         // This is the theme of your application.
         //
